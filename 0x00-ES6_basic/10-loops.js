db4d6cb3215254fdd8a077da2b0f34e066d245fa
kholoudxs55kh <kfattem@gmail.com>
2024-01-02 22:46:41 +0200

    fixing EsLint - adding Adv Tasks

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,4 @@
-// Rewrite the function appendToEachArrayValue to use ES6’s for...of operator. And don’t forget that var is not ES6-friendly.
-
-export default function appendToEachArrayValue (array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   const newArr = [];
 
   for (const value of array) {
--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,12 +1,12 @@
 import getBudgetObject from './7-getBudgetObject.js';
 // Rewrite getFullBudgetObject to use ES6 method properties in the fullBudget object
 
-export default function getFullBudgetObject (income, gdp, capita) {
+export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
     getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuros: (income) => `${income} euros`
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
