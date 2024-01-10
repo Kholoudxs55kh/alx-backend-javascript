50eb8e76f96d073389a4785e6730502fa64f1361
kholoudxs55kh <kfattem@gmail.com>
2024-01-10 06:21:02 +0200

    5th task

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,20 @@
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName[0], ". ").concat(lastName);
+};
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
+console.log(printTeacher("kholoud", "fattem"));
+var student = new StudentClass("kholoud", "fattem");
+console.log(student.displayName());
+console.log(student.workOnHomework());
